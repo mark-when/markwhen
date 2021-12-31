34c71d973fedcb7de8893c1a5ed747c49600c3c5
Rob Koch <kochrt@gmail.com>
2021-12-30 16:50:51 -0800

    Implement panning

--- components/Timeline/Events.vue
+++ components/Timeline/Events.vue
@@ -1,5 +1,10 @@
 <template>
-  <div id="events" class="flex flex-col relative">
+  <div
+    id="events"
+    class="flex flex-col relative"
+    @mousedown="panStart"
+    :style="eventsStyle"
+  >
     <years :years="years" :columnWidth="columnWidth" />
     <div class="h-24"></div>
     <!-- <transition-group
@@ -34,6 +39,57 @@ export default Vue.extend({
     transitionName(): string {
       return "eventRow";
     },
+    eventsStyle(): string {
+      return `cursor: ${this.panStartX ? "grabbing" : "grab"};`;
+    },
+  },
+  data() {
+    return {
+      panStartX: null as number | null,
+      panStartY: null as number | null,
+      startingScrollLeft: null as number | null,
+      startingScrollTop: null as number | null,
+    };
+  },
+  methods: {
+    panStart(e: MouseEvent) {
+      if (this.panStartX === null) {
+        this.startingScrollLeft = this.$el.parentElement!.scrollLeft;
+        this.startingScrollTop = this.$el.parentElement!.scrollTop;
+        this.panStartX = e.clientX;
+        this.panStartY = e.clientY;
+      }
+      window.addEventListener("mousemove", this.panning);
+      window.addEventListener("mouseup", this.endPanning);
+    },
+    panning(e: MouseEvent) {
+      this.$el.parentElement!.scrollLeft =
+        this.startingScrollLeft! + this.panStartX! - e.clientX;
+      this.$el.parentElement!.scrollTop =
+        this.startingScrollTop! + this.panStartY! - e.clientY;
+    },
+    endPanning(e: MouseEvent) {
+      this.panStartX = null;
+      this.panStartY = null;
+      window.removeEventListener("mousemove", this.panning);
+      window.removeEventListener("mouseup", this.endPanning);
+    },
+    // pan(e) {
+    //   // if (e.pointerType !== "mouse") {
+    //   //   return false;
+    //   // }
+    //   if (this.panStartX === null) {
+    //     this.panStartX = this.$el.parentElement!.scrollLeft;
+    //     this.panStartY = this.$el.parentElement!.scrollTop;
+    //   }
+    //   this.$el.parentElement!.scrollLeft = this.panStartX - e.deltaX;
+    //   this.$el.parentElement!.scrollTop = this.panStartY - e.deltaY;
+    //   if (e.isFinal) {
+    //     this.panStartX = null;
+    //     this.panStartY = null;
+    //   }
+    //   console.log(e);
+    // },
   },
 });
 </script>
