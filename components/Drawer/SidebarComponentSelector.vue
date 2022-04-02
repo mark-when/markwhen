b23ae4771a231014aa6bb8d76376881e97a97ceb
Rob Koch <kochrt@gmail.com>
2022-04-02 10:56:14 -0700

    Add regexes for relative times

--- components/Drawer/SidebarComponentSelector.vue
+++ components/Drawer/SidebarComponentSelector.vue
@@ -36,7 +36,8 @@
         ></path>
       </svg>
     </button>
-    <!-- <button
+    <button
+      v-if="debug"
       class="p-2 hover:bg-gray-900 md:mb-3 mb-0 mr-3 md:mr-0"
       :class="selectedComponent === 'explore' ? 'bg-gray-900' : ''"
       @click="selectExplore"
@@ -53,7 +54,7 @@
           d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5 7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z"
         ></path>
       </svg>
-    </button> -->
+    </button>
     <button class="p-2" @click="selectHowTo" title="Help and How to">
       <svg
         class="h-6 w-6 rounded-full"
@@ -76,6 +77,11 @@ import Vue from "vue";
 import { mapState } from "vuex";
 
 export default Vue.extend({
+  data() {
+    return {
+      debug: this.$config.dev,
+    };
+  },
   computed: mapState({
     selectedComponent: (state: any) =>
       state.sidebar.selectedComponent as string,
--- src/Parser.ts
+++ src/Parser.ts
@@ -1,34 +1,71 @@
-import { DateTime } from "luxon"
-import sortEvents, { Sort, EventSubGroup } from "./Sort"
-import { Cascade, DateRange, Event, EventDescription, Tags } from "./Types"
+import { DateTime } from "luxon";
+import sortEvents, { Sort, EventSubGroup } from "./Sort";
+import { Cascade, DateRange, Event, EventDescription, Tags } from "./Types";
 
-const ISO8601_REGEX = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2,}(?:\.\d*)?Z/
-const NOW_REGEX = /now/
-const DATE_REGEX = /\d{1,5}(\/\d{1,5}(\/\d{1,5})?)?/
+// Amounts
+const MILLISECOND_AMOUNT_REGEX = /(milliseconds|ms)/i;
+const SECOND_AMOUNT_REGEX = /(seconds|second|secs|sec|s)/i;
+const MINUTE_AMOUNT_REGEX = /(minutes|minute|mins|min)/i;
+const HOUR_AMOUNT_REGEX = /(hours|hour|h)/i;
+const DAY_AMOUNT_REGEX = /(days|day|d)/i;
+const WEEK_AMOUNT_REGEX = /(weeks|week|w)/i;
+const MONTH_AMOUNT_REGEX = /(months|month)/i;
+const YEAR_AMOUNT_REGEX = /(years|year|y)/i;
+const DECADE_AMOUNT_REGEX = /(decades|decade)/i;
+const AMOUNT_REGEX = new RegExp(
+  `(\\d+\\W*)(${MILLISECOND_AMOUNT_REGEX.source}|${SECOND_AMOUNT_REGEX.source}|${MINUTE_AMOUNT_REGEX.source}|${HOUR_AMOUNT_REGEX.source}|${DAY_AMOUNT_REGEX.source}|${WEEK_AMOUNT_REGEX.source}|${MONTH_AMOUNT_REGEX.source}|${YEAR_AMOUNT_REGEX.source}|${DECADE_AMOUNT_REGEX.source})(?:\\s*,\\s*|\\s*)`
+);
 
-const START_OR_END_TIME_REGEX = new RegExp(`(${ISO8601_REGEX.source})|(${NOW_REGEX.source})|(${DATE_REGEX.source})`)
+const EVENT_ID_REGEX = /!\w+/;
 
-const DATE_RANGE_REGEX = new RegExp(`((${START_OR_END_TIME_REGEX.source})(?:\\s*-\\s*(${START_OR_END_TIME_REGEX.source}))?):`)
-const EVENT_START_REGEX = new RegExp(`(\\s*)${DATE_RANGE_REGEX.source}(.*)`)
-const COMMENT_REGEX = /^\s*\/\/.*/
-const TAG_COLOR_REGEX = /^\s*#(\w*):\s*(\S+)/
-const DATE_FORMAT_REGEX = /dateFormat:\s*d\/M\/y/
-const TAG_REGEX = /(?:^| )#(\w*)/g
-const GROUP_START_REGEX = /^(\s*)group/
-const GROUP_END_REGEX = /^endGroup/
+// So this regex is kind of wrong - we're using the global flag here to make multiple matches for the
+// whole regex, even though we just want any repeated amounts (e.g., 3 days, 4 hours, 6 seconds). 
+// This works because the entire front part (`after !eventId plus`) is optional
+const RELATIVE_TIME_REGEX = new RegExp(
+  `((before|after)?\\s*${EVENT_ID_REGEX.source}\\s*)?(?:plus|\\+)?\\s*${AMOUNT_REGEX.source}`,
+  "g"
+);
+console.log(RELATIVE_TIME_REGEX.source);
 
-// I apologize, this is just GROUP_START_REGEX combined with DATE_RANGE_REGEX. 
+const ISO8601_REGEX = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2,}(?:\.\d*)?Z/;
+const NOW_REGEX = /now/;
+const DATE_REGEX = /\d{1,5}(\/\d{1,5}(\/\d{1,5})?)?/;
+
+const START_OR_END_TIME_REGEX = new RegExp(
+  `(${ISO8601_REGEX.source})|(${NOW_REGEX.source})|(${DATE_REGEX.source})`
+);
+
+const DATE_RANGE_REGEX = new RegExp(
+  `((${START_OR_END_TIME_REGEX.source})(?:\\s*-\\s*(${START_OR_END_TIME_REGEX.source}))?):`
+);
+const EVENT_START_REGEX = new RegExp(`(\\s*)${DATE_RANGE_REGEX.source}(.*)`);
+
+const COMMENT_REGEX = /^\s*\/\/.*/;
+const TAG_COLOR_REGEX = /^\s*#(\w*):\s*(\S+)/;
+const DATE_FORMAT_REGEX = /dateFormat:\s*d\/M\/y/;
+const TAG_REGEX = /(?:^| )#(\w*)/g;
+const GROUP_START_REGEX = /^(\s*)group/;
+const GROUP_END_REGEX = /^endGroup/;
+
+// I apologize, this is just GROUP_START_REGEX combined with DATE_RANGE_REGEX.
 // It wasn't working well when I tried to do new RegExp(...)
 // const GROUP_START_WITH_RANGE_REGEX = /^(\s*)group(\s)*((((\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2,}(?:\.\d*)?Z)|(now)|(\d{1,5}(\d{1,5}(\/\d{1,5})?)?))(?:\s*-\s*((\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2,}(?:\.\d*)?Z)|(now)|(\d{1,5}(\/\d{1,5}(\/\d{1,5})?)?)))?):)?(.*)/
 
-export const COLORS = ["green", "blue", "red", "yellow", "indigo", "purple", "pink"];
-export const sorts = ["none", "down", "up"]
+export const COLORS = [
+  "green",
+  "blue",
+  "red",
+  "yellow",
+  "indigo",
+  "purple",
+  "pink",
+];
+export const sorts = ["none", "down", "up"];
 
-const AMERICAN_DATE_FORMAT = 'M/d/y'
-const EUROPEAN_DATE_FORMAT = 'd/M/y'
+const AMERICAN_DATE_FORMAT = "M/d/y";
+const EUROPEAN_DATE_FORMAT = "d/M/y";
 
 export function parse(eventsString?: string, sort: Sort = "none"): Cascade {
-
   if (!eventsString) {
     return {
       events: [],
@@ -36,146 +73,161 @@ export function parse(eventsString?: string, sort: Sort = "none"): Cascade {
       metadata: {
         earliestTime: DateTime.now().minus({ years: 5 }),
         latestTime: DateTime.now().plus({ years: 5 }),
-        dateFormat: AMERICAN_DATE_FORMAT
-      }
-    }
+        dateFormat: AMERICAN_DATE_FORMAT,
+      },
+    };
   }
 
-  const lines = eventsString.split('\n')
-  const events = [] as (Event | EventSubGroup)[]
-  const tags = {} as Tags
-  let paletteIndex = 0
-  let dateFormat = AMERICAN_DATE_FORMAT
-  let earliest: DateTime | null = null, latest: DateTime | null = null
+  const lines = eventsString.split("\n");
+  const events = [] as (Event | EventSubGroup)[];
+  const tags = {} as Tags;
+  let paletteIndex = 0;
+  let dateFormat = AMERICAN_DATE_FORMAT;
+  let earliest: DateTime | null = null,
+    latest: DateTime | null = null;
 
   // For events that are indented and/or grouped
-  let eventSubgroup: EventSubGroup | undefined = undefined
+  let eventSubgroup: EventSubGroup | undefined = undefined;
 
   for (let i = 0; i < lines.length; i++) {
     // Remove comments
-    const line = lines[i]
+    const line = lines[i];
     if (line.match(COMMENT_REGEX)) {
-      continue
+      continue;
     }
-    const tagColorMatch = line.match(TAG_COLOR_REGEX)
+    const tagColorMatch = line.match(TAG_COLOR_REGEX);
     if (tagColorMatch) {
-      tags[tagColorMatch[1]] = tagColorMatch[2]
-      continue
+      tags[tagColorMatch[1]] = tagColorMatch[2];
+      continue;
     }
     if (line.match(DATE_FORMAT_REGEX)) {
-      dateFormat = EUROPEAN_DATE_FORMAT
-      continue
+      dateFormat = EUROPEAN_DATE_FORMAT;
+      continue;
     }
-    const matches = line.matchAll(TAG_REGEX)
+    const matches = line.matchAll(TAG_REGEX);
     if (matches) {
       for (let m of matches) {
         if (!tags[m[1]]) {
-          tags[m[1]] = COLORS[paletteIndex++ % COLORS.length]
+          tags[m[1]] = COLORS[paletteIndex++ % COLORS.length];
         }
       }
     }
 
-    const groupStart = line.match(GROUP_START_REGEX)
+    const groupStart = line.match(GROUP_START_REGEX);
     if (groupStart) {
       // We're starting a new group here. If there was a previous group, end it and
       // push it.
       if (eventSubgroup) {
-        events.push(eventSubgroup)
+        events.push(eventSubgroup);
       }
-      eventSubgroup = parseGroupFromStartTag(line, groupStart)
-      continue
+      eventSubgroup = parseGroupFromStartTag(line, groupStart);
+      continue;
     }
 
     if (eventSubgroup && line.match(GROUP_END_REGEX)) {
       // We are ending our subgroup
-      events.push(eventSubgroup)
-      eventSubgroup = undefined
-      continue
+      events.push(eventSubgroup);
+      eventSubgroup = undefined;
+      continue;
     }
 
-    const eventStart = line.match(EVENT_START_REGEX)
+    const eventStart = line.match(EVENT_START_REGEX);
     if (eventStart) {
-      let end = i
-      let nextLine
+      let end = i;
+      let nextLine;
       do {
-        nextLine = lines[++end]
+        nextLine = lines[++end];
       } while (
-        typeof nextLine === 'string'
-        && !nextLine.match(EVENT_START_REGEX)
-        && !nextLine.match(GROUP_START_REGEX)
-        && !(eventSubgroup && nextLine.match(GROUP_END_REGEX))
-      )
+        typeof nextLine === "string" &&
+        !nextLine.match(EVENT_START_REGEX) &&
+        !nextLine.match(GROUP_START_REGEX) &&
+        !(eventSubgroup && nextLine.match(GROUP_END_REGEX))
+      );
 
-      const eventGroup = lines.slice(i, end).filter(l => l && !l.match(COMMENT_REGEX))
+      const eventGroup = lines
+        .slice(i, end)
+        .filter((l) => l && !l.match(COMMENT_REGEX));
 
       // Initial date range and first line description
-      const firstLine = eventGroup[0]
+      const firstLine = eventGroup[0];
 
       // What the regex matched as the date range part
-      const datePart = eventStart[2]
-      const eventStartDate = eventStart[3] || eventStart[6]
-      const eventEndDate = eventStart[9] || eventStart[12]
+      const datePart = eventStart[2];
+      const eventStartDate = eventStart[3] || eventStart[6];
+      const eventEndDate = eventStart[9] || eventStart[12];
 
       // Remove the date part from the first line
-      eventGroup[0] = eventGroup[0].substring(eventGroup[0].indexOf(datePart) + datePart.length + 1).trim()
-      const dateRange = new DateRange(eventStartDate, eventEndDate, datePart, dateFormat)
-      const eventDescription = new EventDescription(eventGroup)
-      const event = new Event(firstLine, dateRange, eventDescription)
+      eventGroup[0] = eventGroup[0]
+        .substring(eventGroup[0].indexOf(datePart) + datePart.length + 1)
+        .trim();
+      const dateRange = new DateRange(
+        eventStartDate,
+        eventEndDate,
+        datePart,
+        dateFormat
+      );
+      const eventDescription = new EventDescription(eventGroup);
+      const event = new Event(firstLine, dateRange, eventDescription);
 
       if (event) {
         if (eventSubgroup) {
-          eventSubgroup.push(event)
+          eventSubgroup.push(event);
         } else {
-          events.push(event)
+          events.push(event);
         }
 
         if (!earliest || dateRange.fromDateTime < earliest) {
-          earliest = dateRange.fromDateTime
+          earliest = dateRange.fromDateTime;
         }
         if (!latest || dateRange.toDateTime > latest) {
-          latest = dateRange.toDateTime
+          latest = dateRange.toDateTime;
         }
       }
     }
   }
 
   if (eventSubgroup) {
-    events.push(eventSubgroup)
+    events.push(eventSubgroup);
   }
 
   if (!earliest) {
-    earliest = DateTime.now().minus({ years: 5 })
+    earliest = DateTime.now().minus({ years: 5 });
   }
   if (!latest) {
-    latest = DateTime.now().plus({ years: 5 })
+    latest = DateTime.now().plus({ years: 5 });
   }
-  sortEvents(events, sort)
+  sortEvents(events, sort);
   return {
-    events, tags, metadata: {
+    events,
+    tags,
+    metadata: {
       earliestTime: earliest,
       latestTime: latest,
-      dateFormat
-    }
-  }
+      dateFormat,
+    },
+  };
 }
 
-function parseGroupFromStartTag(s: string, regexMatch: RegExpMatchArray): EventSubGroup {
-  const group: EventSubGroup = []
-  group.tags = []
+function parseGroupFromStartTag(
+  s: string,
+  regexMatch: RegExpMatchArray
+): EventSubGroup {
+  const group: EventSubGroup = [];
+  group.tags = [];
 
   s = s
     .replace(GROUP_START_REGEX, (match, startToken) => {
       // Start expanded if this start tag is not indented
-      group.startExpanded = !startToken.length
-      return ''
+      group.startExpanded = !startToken.length;
+      return "";
     })
     .replace(TAG_REGEX, (match, tag) => {
       if (!group.tags!.includes(tag)) {
-        group.tags!.push(tag)
+        group.tags!.push(tag);
       }
-      return ''
-    })
+      return "";
+    });
 
-  group.title = s.trim()
-  return group
+  group.title = s.trim();
+  return group;
 }
