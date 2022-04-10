3f9b6101921666ee93dd8fe0b9ecf1308a8a54d5
Rob Koch <kochrt@gmail.com>
2022-04-10 10:06:31 -0700

    Format nuxt.config.js, check if we already have a firebase app before initializing

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,36 +2,45 @@ export default {
   loading: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Cascade.page',
+    title: "Cascade.page",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no' },
-      { hid: 'description', name: 'description', content: "Create a timeline of meaningful events" },
-      { name: 'format-detection', content: 'telephone=no' },
-      { property: 'og:image', content: "https://api.sw.ink/v0/swink/761QQ2NQBM/qr" },
-      { property: 'description', content: "Create a timeline of meaningful events" }
+      { charset: "utf-8" },
+      {
+        name: "viewport",
+        content:
+          "width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no",
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "Create a timeline of meaningful events",
+      },
+      { name: "format-detection", content: "telephone=no" },
+      {
+        property: "og:image",
+        content: "https://api.sw.ink/v0/swink/761QQ2NQBM/qr",
+      },
+      {
+        property: "description",
+        content: "Create a timeline of meaningful events",
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
   },
 
   server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || '3001'
+    host: "0.0.0.0",
+    port: process.env.PORT || "3001",
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: "~/plugins/rpc", mode: 'client' },
-  ],
+  plugins: [{ src: "~/plugins/rpc", mode: "client" }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -39,26 +48,26 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
   ],
 
   serverMiddleware: {
-    '/': '~/server-middleware/index'
+    "/": "~/server-middleware/index",
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    browserBaseUrl: '/',
+    browserBaseUrl: "/",
     proxyHeaders: false,
-    credentials: false
+    credentials: false,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -67,6 +76,6 @@ export default {
   },
 
   publicRuntimeConfig: {
-    dev: process.env.DEV
-  }
-}
+    dev: process.env.DEV,
+  },
+};
--- server-middleware/index.js
+++ server-middleware/index.js
@@ -1,14 +1,16 @@
-import * as admin from "firebase-admin"
-import * as express from "express"
-const app = express.default()
-app.use(express.json())
+import * as admin from "firebase-admin";
+import * as express from "express";
+const app = express.default();
+app.use(express.json());
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: "https:/timelinecascade.firebaseio.com",
-  storageBucket: "timelinecascade.appspot.com",
-  projectId: "timelinecascade"
-});
+if (!admin.apps || !admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: "https:/timelinecascade.firebaseio.com",
+    storageBucket: "timelinecascade.appspot.com",
+    projectId: "timelinecascade",
+  });
+}
 
 // interface Request extends express.Request {
 //   auth?: any
@@ -17,121 +19,154 @@ admin.initializeApp({
 // }
 
 function failUnauthorized(res, message) {
-  let response =
-    {
-      error: 'Unauthorized.'
-    } 
+  let response = {
+    error: "Unauthorized.",
+  };
   if (message) {
-    response.message = message
+    response.message = message;
   }
-  res.status(401).json(response).end()
+  res
+    .status(401)
+    .json(response)
+    .end();
 }
 
-const chooseUserName = '/api/chooseUserName'
-const share = '/api/share'
-const explore = '/api/explore'
-const cascade = '/api/cascade/:username/:cascade?'
-const requiresAuth = [chooseUserName, share]
+const chooseUserName = "/api/chooseUserName";
+const share = "/api/share";
+const explore = "/api/explore";
+const cascade = "/api/cascade/:username/:cascade?";
+const requiresAuth = [chooseUserName, share];
 
 app.use(requiresAuth, async (req, res, next) => {
-  let token
+  let token;
   if (req.headers && req.headers.authorization) {
-    const parts = req.headers.authorization.split(' ');
-    if (parts.length === 2 && parts[0] === 'Bearer') {
+    const parts = req.headers.authorization.split(" ");
+    if (parts.length === 2 && parts[0] === "Bearer") {
       token = parts[1];
     } else {
-      return failUnauthorized(res, "Unable to parse authorization token.")
+      return failUnauthorized(res, "Unable to parse authorization token.");
     }
   } else {
-    return failUnauthorized(res, "Missing authorization token in headers.")
+    return failUnauthorized(res, "Missing authorization token in headers.");
   }
-  let verifiedToken
+  let verifiedToken;
   try {
-    verifiedToken = await admin.auth().verifyIdToken(token)
+    verifiedToken = await admin.auth().verifyIdToken(token);
   } catch (error) {
-    console.error(error)
-    return failUnauthorized(res, "Unable to verify token.")
+    console.error(error);
+    return failUnauthorized(res, "Unable to verify token.");
   }
-  req.auth = verifiedToken
-  next()
-})
+  req.auth = verifiedToken;
+  next();
+});
 
-const RESERVED_USERNAMES = ['api', 'assets', 'from']
+const RESERVED_USERNAMES = ["api", "assets", "from"];
 app.post(chooseUserName, async (req, res) => {
-  const username = req.body.username
+  const username = req.body.username;
   if (!username) {
-    return res.status(400).json({
-      error: "Missing username"
-    }).end()
+    return res
+      .status(400)
+      .json({
+        error: "Missing username",
+      })
+      .end();
   }
 
   if (!username.match(/^[\w\d_-]+$/)) {
-    return res.status(400).json({
-      error: "Usernames can only have letters, digits, underscores, and dashes."
-    }).end()
+    return res
+      .status(400)
+      .json({
+        error:
+          "Usernames can only have letters, digits, underscores, and dashes.",
+      })
+      .end();
   }
 
   if (RESERVED_USERNAMES.includes(username) || username.startsWith("_")) {
-    return res.status(400).json({
-      error: "Username unavailable."
-    }).end()
+    return res
+      .status(400)
+      .json({
+        error: "Username unavailable.",
+      })
+      .end();
   }
 
   try {
-    await admin.firestore().runTransaction(async transaction => {
+    await admin.firestore().runTransaction(async (transaction) => {
       const usernameInUse = await transaction.get(
-        admin.firestore().collection('users').where('username', '==', username)
-      )
+        admin
+          .firestore()
+          .collection("users")
+          .where("username", "==", username)
+      );
       if (!usernameInUse.empty) {
-        console.error("username already in use", username)
-        throw new Error('Username already in use')
+        console.error("username already in use", username);
+        throw new Error("Username already in use");
       }
-      const uid = req.auth.uid
-      transaction.set(admin.firestore().doc(`users/${uid}`), { username }, { merge: true })
-    })
-    return res.status(200).json({ username })
+      const uid = req.auth.uid;
+      transaction.set(
+        admin.firestore().doc(`users/${uid}`),
+        { username },
+        { merge: true }
+      );
+    });
+    return res.status(200).json({ username });
   } catch (err) {
-    console.error(err)
-    return res.status(409).json({ error: "Username already in use" })
+    console.error(err);
+    return res.status(409).json({ error: "Username already in use" });
   }
-})
+});
 
 app.post(share, async (req, res) => {
-  const timeline = req.body.timeline
+  const timeline = req.body.timeline;
   if (!timeline) {
-    return res.status(400).json({
-      error: "Missing timeline"
-    }).end()
+    return res
+      .status(400)
+      .json({
+        error: "Missing timeline",
+      })
+      .end();
   }
 
-  const name = req.body.name
+  const name = req.body.name;
   if (!name) {
-    return res.status(400).json({
-      error: "Missing timeline name"
-    }).end()
+    return res
+      .status(400)
+      .json({
+        error: "Missing timeline name",
+      })
+      .end();
   }
 
   if (!name.match(/^[\w\d_-]+$/)) {
-    return res.status(400).json({
-      error: "Timeline names can only contain letters, digits, underscores, and dashes."
-    }).end()
+    return res
+      .status(400)
+      .json({
+        error:
+          "Timeline names can only contain letters, digits, underscores, and dashes.",
+      })
+      .end();
   }
 
-  const timestamp = Date.now()
+  const timestamp = Date.now();
 
-  const path = `${req.auth.uid}/${req.body.name}`
-  const file = admin.storage().bucket().file(path)
+  const path = `${req.auth.uid}/${req.body.name}`;
+  const file = admin
+    .storage()
+    .bucket()
+    .file(path);
   await file.save(req.body.timeline, {
-    contentType: "text/plain", metadata: {
-      lastEdited: timestamp
-    }
-  })
-  return res.status(200).send()
-})
+    contentType: "text/plain",
+    metadata: {
+      lastEdited: timestamp,
+    },
+  });
+  return res.status(200).send();
+});
 
 async function getAllUserIds() {
-  const users = await admin.auth().listUsers()
-  return users.users.map(user => user.uid)
+  const users = await admin.auth().listUsers();
+  return users.users.map((user) => user.uid);
 }
 
 // interface UserAndId {
@@ -140,70 +175,94 @@ async function getAllUserIds() {
 // }
 
 async function getAllUserNamesAndIds() {
-  const users = (await getAllUserIds()).map(userId => admin.firestore().doc(`users/${userId}`))
-  const usernames = await admin.firestore().getAll(...users, { fieldMask: ['username'] })
-  return usernames.filter(userDoc => userDoc.exists && !!userDoc.data().username).map(userDoc => {
-    return {
-      username: userDoc.data().username,
-      userId: userDoc.id
-    }
-  })
+  const users = (await getAllUserIds()).map((userId) =>
+    admin.firestore().doc(`users/${userId}`)
+  );
+  const usernames = await admin
+    .firestore()
+    .getAll(...users, { fieldMask: ["username"] });
+  return usernames
+    .filter((userDoc) => userDoc.exists && !!userDoc.data().username)
+    .map((userDoc) => {
+      return {
+        username: userDoc.data().username,
+        userId: userDoc.id,
+      };
+    });
 }
 
 app.get(explore, async (req, res) => {
-  const users = await getAllUserNamesAndIds()
-  const allFiles = await users.map(async user => {
-    const [files] =
-      await admin.storage().bucket().getFiles({ prefix: user.userId })
-    return files.map(f => {
-      let cascadePath
+  const users = await getAllUserNamesAndIds();
+  const allFiles = await users.map(async (user) => {
+    const [files] = await admin
+      .storage()
+      .bucket()
+      .getFiles({ prefix: user.userId });
+    return files.map((f) => {
+      let cascadePath;
       if (user.username === f.name.substring(user.userId.length + 1)) {
-        cascadePath = user.username
+        cascadePath = user.username;
       } else {
-        cascadePath = user.username + f.name.substring(user.userId.length)
+        cascadePath = user.username + f.name.substring(user.userId.length);
       }
       return {
-        cascadePath, filePath: f.name
-      }
-    })
-  })
+        cascadePath,
+        filePath: f.name,
+      };
+    });
+  });
 
-  const files = await (await Promise.all(allFiles)).flatMap(f => f)
-  res.status(200).json({ cascades: files })
-})
+  const files = await (await Promise.all(allFiles)).flatMap((f) => f);
+  res.status(200).json({ cascades: files });
+});
 
 app.get(cascade, async (req, res) => {
-  const path = `${req.params.username}/${req.params.cascade ? req.params.cascade : req.params.username}`
-  const file = await admin.storage().bucket().file(path)
-  const [exists] = await file.exists()
+  const path = `${req.params.username}/${
+    req.params.cascade ? req.params.cascade : req.params.username
+  }`;
+  const file = await admin
+    .storage()
+    .bucket()
+    .file(path);
+  const [exists] = await file.exists();
   if (!exists) {
-    return res.status(404).send()
+    return res.status(404).send();
   }
-  res.contentType('text/html')
-  res.status(200)
-  file.createReadStream().pipe(res)
-})
+  res.contentType("text/html");
+  res.status(200);
+  file.createReadStream().pipe(res);
+});
 
-app.get('/:user/:timeline?', async (req, res, next) => {
-  const user = await admin.firestore().collection(`users`).where('username', '==', req.params.user).get()
+app.get("/:user/:timeline?", async (req, res, next) => {
+  const user = await admin
+    .firestore()
+    .collection(`users`)
+    .where("username", "==", req.params.user)
+    .get();
   if (!user || user.empty) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
   if (user.size > 1) {
-    console.warn('More than one user with this username!', req.params.user)
+    console.warn("More than one user with this username!", req.params.user);
   }
-  const userId = user.docs[0].id
-  const path = `${userId}/${req.params.timeline ? req.params.timeline : req.params.user}`
-  const file = admin.storage().bucket().file(path)
-  const exists = await file.exists()
+  const userId = user.docs[0].id;
+  const path = `${userId}/${
+    req.params.timeline ? req.params.timeline : req.params.user
+  }`;
+  const file = admin
+    .storage()
+    .bucket()
+    .file(path);
+  const exists = await file.exists();
   if (!exists[0]) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
-  const timelineFile = await file.download()
-  req.timelineFile = timelineFile.toString()
-  req.timelinePath = `${req.params.user}${req.params.timeline ? ('/' + req.params.timeline) : ''}`
-  next()
-})
+  const timelineFile = await file.download();
+  req.timelineFile = timelineFile.toString();
+  req.timelinePath = `${req.params.user}${
+    req.params.timeline ? "/" + req.params.timeline : ""
+  }`;
+  next();
+});
 
-
-module.exports = app
+module.exports = app;
