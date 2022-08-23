9fe5f04e9d8cccb6e8b1647e950431206c1dda60
Rob Koch <kochrt@gmail.com>
2022-08-23 15:27:49 -0700

    Convert editorOrchestratorStore to setup store

--- src/EditorOrchestrator/editorOrchestratorStore.ts
+++ src/EditorOrchestrator/editorOrchestratorStore.ts
@@ -2,6 +2,7 @@ import { useMarkwhenStore } from "@/Markwhen/markwhenStore";
 import { defineStore } from "pinia";
 import { PAGE_BREAK } from "@markwhen/parser/lib/regex";
 import type { Event, Timeline } from "@markwhen/parser/lib/Types";
+import { ref } from "vue";
 
 export const ADD_PAGE = "edit:pages:add";
 export const MOVE_PAGES = "edit:pages:move";
@@ -18,26 +19,33 @@ export type UpdateMethod =
   | typeof CREATE_EVENT_FROM_RANGE
   | typeof HOVER_EVENT;
 
-export const useEditorOrchestratorStore = defineStore("editorOrchestrator", {
-  state: () => ({
-    editable: true,
-    showTagFilterButtons: true,
-    showPageButtons: true,
-    hoveringEvent: null as Event | null,
-  }),
-  actions: {
-    update(updateMethod: UpdateMethod, args?: any) {
+export const useEditorOrchestratorStore = defineStore(
+  "editorOrchestrator",
+  () => {
+    const editable = ref(true);
+    const showTagFilterButtons = ref(true);
+    const showPageButtons = ref(true);
+    const hoveringEvent = ref<Event | null>(null);
+
+    const update = (updateMethod: UpdateMethod, arg?: any) => {
       if (updateMethod === HOVER_EVENT) {
-        if (args) {
-          this.hoveringEvent = args;
-        } else {
-          this.hoveringEvent = null;
-        }
+        hoveringEvent.value = arg ? arg : null;
       }
-      console.log(updateMethod, args);
-    },
-  },
-});
+      console.log(updateMethod, arg);
+    };
+
+    return {
+      // state
+      editable,
+      showTagFilterButtons,
+      showPageButtons,
+      hoveringEvent,
+
+      // actions
+      update
+    };
+  }
+);
 
 const editorTransformer = (
   timelineString: string,
