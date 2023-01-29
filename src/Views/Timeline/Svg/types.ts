7decd4ad802406603c89c01bda9f702b565c9f1b
Rob Koch <kochrt@gmail.com>
2023-01-29 08:12:51 -0500

    Delete timeline view

--- src/Views/Timeline/Svg/types.ts
+++
@@ -1,5 +0,0 @@
-import type { DateRange } from "@markwhen/parser/lib/Types"
-
-export type Era = {
-  dateRange: DateRange
-}
--- src/Views/Timeline/composables/useIsTouchscreen.ts
+++
@@ -1,6 +0,0 @@
-import { useMediaQuery } from "@vueuse/core";
-
-export const useIsTouchscreen = () => ({
-  isTouchscreen: useMediaQuery("(pointer: course)"),
-  canHover: useMediaQuery('(hover)')
-});
