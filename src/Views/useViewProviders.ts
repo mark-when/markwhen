ef8603a402ef5b20809dbfcbe3b895799f214996
Rob Koch <kochrt@gmail.com>
2023-03-27 17:46:07 -0400

    Fix a couple more build errors

--- src/Views/useViewProviders.ts
+++ src/Views/useViewProviders.ts
@@ -4,7 +4,7 @@ export const useTimelineExternalProvider = () => ({
   id: "markwhen.timeline",
   name: "Timeline",
   url: "https://timeline.markwhen.com",
-  iconSvg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="h-5 w-5"><path 
+  iconSvg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="h-full w-full"><path 
   fill="currentColor" 
   d="m 13 13 h -5 c -0.55 0 -1 -0.45 -1 -1 s 0.45 -1 1 -1 h 5 c 0.55 0 1 0.45 1 1 s -0.45 1 -1 1 z m -1 -4 h -7 c -0.55 0 -1 -0.45 -1 -1 s 0.45 -1 1 -1 h 7 c 0.55 0 1 0.45 1 1 s -0.45 1 -1 1 z m -9 -4 h 0 c -0.55 0 -1 -0.45 -1 -1 s 0.45 -1 1 -1 h 2 c 0.55 0 1 0.45 1 1 s -0.45 1 -1 1 z">
 </path></svg>`,
--- src/Views/visualizationStore.ts
+++ src/Views/visualizationStore.ts
@@ -1,4 +1,3 @@
-import { useStorageStore } from "@/Storage/storageStore";
 import type { ViewProvider } from "@/viewProvider";
 import { defineStore } from "pinia";
 import { computed, ref, watch, watchEffect } from "vue";
@@ -8,7 +7,6 @@ import { useViewOrchestrator } from "./ViewOrchestrator/useViewOrchestrator";
 const viewAssociationsKey = "viewAssociations";
 
 export const useVisualizationStore = defineStore("visualization", () => {
-  const storageStore = useStorageStore();
   const showingWelcomeViewPicker = ref(true);
   const activeFrame = ref<HTMLIFrameElement>();
   const selectedViewIndex = ref(-1);
@@ -55,85 +53,13 @@ export const useVisualizationStore = defineStore("visualization", () => {
     ) as Record<string, string>;
   };
 
-  const viewAssociationKeyForStorageType = computed(() => {
-    switch (storageStore.storageType) {
-      case "cloud":
-        return storageStore.cloudPath;
-      case "draft":
-        return storageStore.draftName || "draft";
-    }
-  });
-
-  const setViewAssociation = () => {
-    const existingViewAssociations = getExistingViewAssociations();
-    if (!existingViewAssociations) {
-      return;
-    }
-
-    if (viewAssociationKeyForStorageType.value && currentView.value) {
-      const updatedAssociations = {
-        ...existingViewAssociations,
-        [viewAssociationKeyForStorageType.value]: currentView.value.id,
-      };
-      localStorage.setItem(
-        viewAssociationsKey,
-        JSON.stringify(updatedAssociations)
-      );
-    }
-  };
-
-  const getAndApplyViewAssociation = () => {
-    const existingViewAssociations = getExistingViewAssociations();
-    if (!existingViewAssociations) {
-      return;
-    }
-    if (existingViewAssociations && viewAssociationKeyForStorageType.value) {
-      const viewId =
-        existingViewAssociations[viewAssociationKeyForStorageType.value];
-      if (viewId) {
-        const index = viewOptions.value.findIndex((vp) => vp.id === viewId);
-        if (index !== -1) {
-          selectedViewIndex.value = index;
-          showingWelcomeViewPicker.value = false;
-          return true;
-        }
-      }
-    }
-    return false;
-  };
-
-  watch(
-    [
-      () => storageStore.storageType,
-      () => storageStore.draftName,
-      () => storageStore.cloudPath,
-    ],
-    () => {
-      getAndApplyViewAssociation();
-      setViewAssociation();
-    },
-    { immediate: true }
-  );
-
-  watch(selectedViewIndex, (val, oldVal) => {
-    if (val !== oldVal) {
-      setViewAssociation();
-    }
-  });
-
   watchEffect(() => {
     if (selectedViewIndex.value >= activeViews.value.length) {
       selectedViewIndex.value = 0;
     } else if (selectedViewIndex.value < 0) {
       // Set default view
       // Don't set view association here because we're just setting the default
-      selectedViewIndex.value = 2;
-    }
-  });
-
-  watch(showingWelcomeViewPicker, (showing) => {
-    if (!showing) {
-      setViewAssociation();
+      selectedViewIndex.value = 0;
     }
   });
 
@@ -141,8 +67,7 @@ export const useVisualizationStore = defineStore("visualization", () => {
     showingWelcomeViewPicker.value = true;
   };
 
-  const { jumpToRange, jumpToPath, getTimelineSvg } =
-    useViewOrchestrator(activeFrame);
+  const { jumpToRange, jumpToPath } = useViewOrchestrator(activeFrame);
 
   return {
     selectedViewIndex,
@@ -152,12 +77,9 @@ export const useVisualizationStore = defineStore("visualization", () => {
     currentView,
     showingWelcomeViewPicker,
 
-    getAndApplyViewAssociation,
-    setViewAssociation,
     showWelcomeViewPicker,
     setActiveFrame,
     jumpToRange,
     jumpToPath,
-    getTimelineSvg,
   };
 });
